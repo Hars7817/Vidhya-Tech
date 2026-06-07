7b1491308464277cb099bd9e2d954d4faca995b7
Harsh7817 <harshkumar123kej@gmail.com>
2026-06-07 15:38:58 +0530

    remove one line

--- app/page.tsx
+++ app/page.tsx
@@ -988,7 +988,7 @@ export default function Home() {
           </motion.div>
         </div>
       </section>
-         <VoiceAgent />
+         {/* <VoiceAgent /> */}
       <Footer />
     </div>
   );
